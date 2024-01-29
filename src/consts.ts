2d1f73b755dc589386ea4855db82d4d701198b7d
Steven Sullivan <stevensullivan256@gmail.com>
2024-01-29 08:19:54 -0800

    feat: add site description

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Stevie's Blog"
-export const SITE_DESCRIPTION = ''
+export const SITE_DESCRIPTION =
+  'A collection of thoughts somewhere at intersection of software, language, and the world around me'
