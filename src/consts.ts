921fff609868b2d0214a5a8d2d6411ef3d1ade7e
Stevie Sullivan <ssullivan@skyslope.com>
2026-01-03 13:11:22 -0800

    chore: corrections

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Stevie's Blog"
+export const SITE_TITLE = 'Poorly Ideated'
 export const SITE_DESCRIPTION =
   'A collection of thoughts somewhere at the intersection of software, language, and the world around me'
